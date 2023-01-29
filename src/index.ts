29ff67c8feed64d2707fa3c6ee15084c8877a3b7
Nickita Lashch <holbootla@gmail.com>
2023-01-29 15:35:45 +0300

    add basic webpack ts configuration

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import {testFunction} from './test';
+
+testFunction('Hello World');
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export function testFunction(value:string):string {
+    console.log(value);
+    return value;
+}

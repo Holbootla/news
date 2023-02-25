f7e4d310a546454f2d242a2fe737da826f3001b6
Nickita Lashch <holbootla@gmail.com>
2023-02-25 13:25:03 +0300

    fix theme classes, fix translations

--- src/shared/ThemeProvider/lib/ThemeContext.tsx
+++ src/shared/ThemeProvider/lib/ThemeContext.tsx
@@ -1,8 +1,8 @@
 import { createContext } from 'react';
 
 export enum Theme {
-    LIGHT = 'light',
-    DARK = 'dark',
+    LIGHT = 'light-theme',
+    DARK = 'dark-theme',
 }
 
 export interface ThemeContextProps {
